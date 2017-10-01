c4a8b02025c63f7b6d7fd46ff95f36570391ca46
benji <benji@altschool.com>
2017-09-30 19:11:20 -0700

    passing the first two tests, verse 99 and verse 89

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,19 @@
 
-$basic_verse = <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-VERSE
+
 
 class Bottles
+
+    def _verse(index)
+      next_index = index - 1 # TODO bounds checking 
+      verse_template = <<-VERSE
+#{index} bottles of beer on the wall, #{index} bottles of beer.
+Take one down and pass it around, #{next_index} bottles of beer on the wall.
+VERSE
+      return verse_template
+    end
  
     def verse(which)
-       return $basic_verse
+      return _verse(which)
+      #return $basic_verse
     end
 end
