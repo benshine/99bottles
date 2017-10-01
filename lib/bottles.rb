a38166f6c142d4ee9f255b57e414b02cbd21fe81
benji <benji@altschool.com>
2017-09-30 19:27:16 -0700

    5 passing tests

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,14 +4,28 @@
 class Bottles
 
     def _verse(index)
-      next_index = index - 1
+      if index > 0
+        return _basic_verse(index)
+      else
+        return _final_verse
+      end
+    end
+
+    def _basic_verse(index)
       count_now_description = bottles_description(index)
-    
+      next_index = index - 1      
       verse_template = <<-VERSE
 #{count_now_description} of beer on the wall, #{count_now_description} of beer.
 Take #{article_for_verse(index)} down and pass it around, #{bottles_description(next_index)} of beer on the wall.
 VERSE
       return verse_template
+    end
+
+    def _final_verse
+      return <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+VERSE
     end
 
     def article_for_verse(index)
