d3518a913996e6c6cbe59415fa06bd7499effe2a
benji <benji@altschool.com>
2017-09-30 19:17:41 -0700

    passing 3 tests

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,13 +7,17 @@ class Bottles
       next_index = index - 1 # TODO bounds checking 
       verse_template = <<-VERSE
 #{index} bottles of beer on the wall, #{index} bottles of beer.
-Take one down and pass it around, #{next_index} bottles of beer on the wall.
+Take one down and pass it around, #{bottles_left_description(next_index)} on the wall.
 VERSE
       return verse_template
+    end
+
+    def bottles_left_description(num_left)
+      suffix = "s" if num_left > 1
+      return "#{num_left} bottle#{suffix} of beer"
     end
  
     def verse(which)
       return _verse(which)
-      #return $basic_verse
     end
 end
